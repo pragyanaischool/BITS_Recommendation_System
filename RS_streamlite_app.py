7464841d73de3464accbed9df3cf9cbf128558c6
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2023-08-16 21:16:19 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1uyUCOJxutgWL2RiXie1RjDiNozQurSQg
 """
 
-import pickle
+from pickle5 import pickle
 import streamlit as st
 import requests
 
